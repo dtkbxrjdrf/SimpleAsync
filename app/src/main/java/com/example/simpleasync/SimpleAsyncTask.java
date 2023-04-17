8946ffb62314a3f3a7cf546f0eb4381e44630efc
Serhii Yatsenko <dtkbxrjdrf@gmail.com>
2023-04-17 12:06:14 +0300

    Phase

--- app/src/main/java/com/example/simpleasync/SimpleAsyncTask.java
+++ app/src/main/java/com/example/simpleasync/SimpleAsyncTask.java
@@ -8,9 +8,10 @@ import android.widget.TextView;
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, Void> {
     private WeakReference<TextView> mTextView;
     private WeakReference<ProgressBar> progressBar;
+    private String result;
 
     SimpleAsyncTask(TextView tv, ProgressBar pb) {
         mTextView = new WeakReference<>(tv);
@@ -18,7 +19,7 @@ public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
     }
 
     @Override
-    protected String doInBackground(Void... voids) {
+    protected Void doInBackground(Void... voids) {
         Random r = new Random();
         int n = r.nextInt(11);
         int s = n * 10;
@@ -27,12 +28,22 @@ public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
             try {
                 Thread.sleep(s);
                 count++;
+                if (count <= 25) {
+                    result = "Napping";
+                } else if (count <= 50) {
+                    result = "Dream";
+                } else if (count <= 75) {
+                    result = "Deep sleep";
+                } else {
+                    result = "Waking up";
+                    }
                 publishProgress(count);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        return "Awake at last after sleeping for " + s + " milliseconds!";
+        result = "Awake at last after sleeping for " + s + " milliseconds!";
+        return null;
     }
 
     @Override
@@ -40,12 +51,12 @@ public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
         super.onProgressUpdate(values);
         int progress = values[0];
         progressBar.get().setProgress(progress);
+        mTextView.get().setText(result);
     }
 
     @Override
-    protected void onPostExecute(String result) {
-        mTextView.get().setText(result);
+    protected void onPostExecute(Void result) {
+        mTextView.get().setText(this.result);
         progressBar.get().setVisibility(View.GONE);
     }
 }
-
